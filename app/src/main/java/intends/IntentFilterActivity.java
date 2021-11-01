db2cf7fe64f2b1751b283233a222724d574aab50
DenisaOSuciu <suciu.denisa33@gmail.com>
2021-11-01 10:16:43 +0200

    LAB3

---
+++ app/src/main/java/intends/IntentFilterActivity.java
@@ -0,0 +1,21 @@
+package intends;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.lifecycles.R;
+
+public class IntentFilterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intent_filter);
+
+        TextView txtView = (TextView) findViewById(R.id.textView);
+        Uri url = getIntent().getData();
+        txtView.setText(url.toString());
+    }
+}
